8bd39c29015229571ef623e5d2201861320f4cc5
Dương Quang Thanh <thanhdqts00628@fpt.edu.vn>
2025-11-13 14:17:54 +0700

    feat: initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ back_end/tradehub_core/src/main/java/com/fisch_tradehub/tradehub_core/TradehubCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fisch_tradehub.tradehub_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradehubCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradehubCoreApplication.class, args);
+	}
+
+}
---
+++ back_end/tradehub_core/src/test/java/com/fisch_tradehub/tradehub_core/TradehubCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisch_tradehub.tradehub_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradehubCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
