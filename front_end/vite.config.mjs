511972dabffafe318e13ab9b3e3828fc639debc1
Dương Quang Vinh <vinhdqts00629@fpt.edu.vn>
2025-11-13 14:06:25 +0700

    fix: add server configuration to allow serving files from project root

--- front_end/vite.config.mjs
+++ front_end/vite.config.mjs
@@ -8,6 +8,12 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        fs: {
+            // Allow serving files from one level up to the project root
+            allow: [".."],
+        },
+    },
     optimizeDeps: {
         noDiscovery: true,
         include: ["yup"],
